4bf64209c3695e3a69afb6e4e7ae46d29182fe53
Your Name <you@example.com>
2020-11-04 20:29:14 +0330

    HSDh

--- term 2/dark shadow5.py
+++ term 2/dark shadow5.py
@@ -1,6 +1,11 @@
 import turtle as tr
 
 
-tr.forward(300)
+tr.forward(30)
 tr.lt(90)
+tr.forward (30)
+tr.lt(90)
+tr.forward(30)
+tr.lt(90)
+tr.forward(30)
 tr.done()
---
+++ term 2/dark shadow9.py
@@ -0,0 +1,5 @@
+string = "nokaretajhydgfxcgufccucgufcuyhcfuycfuycfm soltan"
+lenth = len(string)
+print (lenth)
+for i in range(lenth):
+    print(i)
