3e95e51d023a2ba6fd150de5e24e643b544baa81
merlinrom <bgr.romina@gmail.com>
2021-02-27 17:36:56 +0330

    retet

---
+++ term 2/dark shadow68.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+
+import reg
+
+def register():
+    reg.register(
+        name.get(),
+        last.get(),
+        birth.get(),
+        code.get(),
+    )
+
+
+root = tk.Tk()
+
+tk.Label(root, text='name').grid(row=0, column=0)
+name = tk.Entry(root)
+name.grid(row=0, column=1)
+
+tk.Label(root, text='Last Name').grid(row=1, column=0)
+last = tk.Entry(root)
+last.grid(row=1, column=1)
+
+
+tk.Label(root, text='Birth Date').grid(row=2, column=0)
+birth = tk.Entry(root)
+birth.grid(row=2, column=1)
+
+tk.Label(root, text='ID code').grid(row=3, column=0)
+code = tk.Entry(root)
+code.grid(row=3, column=1)
+
+
+frame = tk.Frame(root)
+frame.grid(row=6, column=0, columnspan=2)
+
+tk.Button(frame, text='register', command=register).grid(row=0, column=0)
+tk.Button(frame, text='cancel', command=root.destroy).grid(row=0, column=1)
+
+
+
+root.mainloop()
