a4f3fb990c763c7258252fe88c61206e94fbc343
merlinrom <bgr.romina@gmail.com>
2021-03-06 17:26:19 +0330

    uqwdfqwg

--- term 2/dark shadow68.py
+++ term 2/dark shadow68.py
@@ -3,9 +3,27 @@ import tkinter.ttk as ttk
 from typing import Text
 import reg
 
-def srch():
-    pass
+def callback(a ,b ,c):
+    c = code.get()
+    e4.config(bg="red")
+    if c.isdigit9:
+        if len(c) == 10:
+            e4.config(bg='green')
+        elif len(c) > 10:
+            code.set(c[:10])
+            e4.config(bg="green")
+    elif len(c) > 10:
+        code.set(v[:10])
 
+
+
+
+def srch():
+    file = open('name.txt', 'r')
+    top = tk.Toplevel()
+    top.geometry(f"{root.winfo_width()}x{root.winfo_height()}")
+    text = tk.Toplevel()
+    text.grid(row=0, column=0)
 
 
 
@@ -16,29 +34,33 @@ def register():
         birth.get(),
         code.get(),
     )
-
-
+    
 root = tk.Tk()
 
-tk.Label(root, text='name').grid(row=0, column=0)
-name = tk.Entry(root)
-name.grid(row=0, column=1)
 
+tk.Label(root, text='name').grid(row=0, column=0)
 tk.Label(root, text='Last Name').grid(row=1, column=0)
-last = tk.Entry(root)
+tk.Label(root, text='Birth Date').grid(row=2, column=0)
+tk.Label(root, text='ID code').grid(row=3, column=0)
+
+name = tk.StringVar()
+e1 = tk.Entry(root, textvarieble=name)
+e1.grid(row=0, column=1)
+
+last = tk.StringVar()
+last = tk.Entry(root, textvarieble=name)
 last.grid(row=1, column=1)
 
-
-tk.Label(root, text='Birth Date').grid(row=2, column=0)
-birth = tk.Entry(root)
+birth = tk.StringVar()
+birth = tk.Entry(root, textvarieble=name)
 birth.grid(row=2, column=1)
 
-tk.Label(root, text='ID code').grid(row=3, column=0)
-code = tk.Entry(root)
+code = tk.StringVar()
+code = tk.Entry(root, textvarieble=name)
 code.grid(row=3, column=1)
 
-
-frame = tk.Frame(root)
+frame = tk.StringVar()
+frame = tk.Frame(root, textvarieble=name)
 frame.grid(row=6, column=0, columnspan=2)
 
 tk.Button(frame, text='register', command=register).grid(row=0, column=0)
@@ -48,6 +70,6 @@ ttk.Separator(root,orient=tk.HORIZONTAL).grid(row=5, column=0, columnspan=2, sti
 tk.label(root, text='sratch').grid(row=6, column=0)
 search = tk.Entry(root)
 search.grid(row=6, column=1)
-tk.Button(root, text='Search, command=srch).grid(row=7, column=0, columnspan=2)
+tk.Button(root, text='Search', command=srch).grid(row=7, column=0, columnspan=2)
 
 root.mainloop()
