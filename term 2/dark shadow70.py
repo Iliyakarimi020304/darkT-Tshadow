a7729f1f40ba600a3e0e244c4b890945f390d8cb
Your Name <you@example.com>
2021-04-07 20:04:33 +0430

    ytyty

--- term 2/dark shadow70.py
+++ term 2/dark shadow70.py
@@ -11,14 +11,15 @@ patients = ttk.Frame(note)
 note.add(patients, text='patients')
 timers = ttk.Frame(note)
 note.add(timers, text="Timers")
-
-
-
-
-
-
-
-
 # ###########################################################
+lf1 = tk.LabelFrame(patients, text="patient 1")
+lf1 = grid(row=0, column=0)
+
+tk.Label(lf1, text='Name').grid(row=0, column=0)
+tk.Label(lf1, text='Time').grid(row=1, column=0)
+name1 = tk.StringVar()
+time1 = tk.StringVar()
+tk.Entery(lf1, textvariable=name1).grid(row=0, column=1)
+tk.Entery(lf1, textvariable=time1).grid(row=0, column=1)
 # ###########################################################
 root.mainloop()
