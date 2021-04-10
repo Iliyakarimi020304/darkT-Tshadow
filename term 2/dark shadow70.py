d69059ed34387c83ce7b06953a75da67d57fc478
Your Name <you@example.com>
2021-04-10 17:29:02 +0430

    asdf

--- term 2/dark shadow70.py
+++ term 2/dark shadow70.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-
+import time
 
 root = tk.Tk()
 
@@ -13,13 +13,53 @@ timers = ttk.Frame(note)
 note.add(timers, text="Timers")
 # ###########################################################
 lf1 = tk.LabelFrame(patients, text="patient 1")
-lf1 = grid(row=0, column=0)
+lf1.grid(row=0, column=0)
 
 tk.Label(lf1, text='Name').grid(row=0, column=0)
 tk.Label(lf1, text='Time').grid(row=1, column=0)
 name1 = tk.StringVar()
 time1 = tk.StringVar()
-tk.Entery(lf1, textvariable=name1).grid(row=0, column=1)
-tk.Entery(lf1, textvariable=time1).grid(row=0, column=1)
+tk.Entry(lf1, textvariable=name1).grid(row=0, column=1)
+tk.Entry(lf1, textvariable=time1).grid(row=0, column=1)
 # ###########################################################
+lf2 = tk.LabelFrame(patients, text="patient 2")
+lf2.grid(row=1, column=0)
+
+tk.Label(lf2, text='Name').grid(row=0, column=0)
+tk.Label(lf2, text='Time').grid(row=1, column=0)
+name2 = tk.StringVar()
+time2 = tk.StringVar()
+tk.Entry(lf2, textvariable=name1).grid(row=0, column=1)
+tk.Entry(lf2, textvariable=time1).grid(row=1, column=1)
+# ###########################################################
+lf3 = tk.LabelFrame(patients, text="patient 3")
+lf3.grid(row=2, column=0)
+
+tk.Label(lf3, text='Name').grid(row=0, column=0)
+tk.Label(lf3, text='Time').grid(row=1, column=0)
+name3 = tk.StringVar()
+time3 = tk.StringVar()
+tk.Entry(lf3, textvariable=name1).grid(row=0, column=1)
+tk.Entry(lf3, textvariable=time1).grid(row=1, column=1)
+
+name3 = tk.StringVar()
+time3 = tk.StringVar()
+# ###########################################################
+j1 = tk.LabelFrame(timers)
+j1.grid(row=0, column=0)
+j1.set('Patient1')
+tk.Label(j1, text='patient 1').grid(row=0, column=0)
+# ###########################################################
+j2 = tk.LabelFrame(timers)
+j2.grid(row=0, column=1)
+
+tk.Label(j2, text='patient 2').grid(row=0, column=0)
+# ###########################################################
+j3 = tk.LabelFrame(timers)
+j3.grid(row=0, column=2)
+
+tk.Label(j3, text='patient 3').grid(row=0, column=0)
+
+
+
 root.mainloop()
